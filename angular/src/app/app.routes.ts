146cc5196d61b10006ce2512d1f93a34a8df8ca9
Matt Beres <mattyberes@gmail.com>
2026-02-12 14:40:47 -0500

    add favicon

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) },
-    { path: 'gallery', loadComponent: () => import('./gallery/gallery.component').then(m => m.GalleryComponent) },
+    { path: 'gallery', loadComponent: () => import('./gallery/gallery.component').then(m => m.GalleryComponent), title: 'BERES Photography - Gallery' },
 ];
